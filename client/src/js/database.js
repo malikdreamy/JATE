f22320adde6c1d855a3f1db1f1895e0d4efffc23
malikdreamy <119153047+malikdreamy@users.noreply.github.com>
2023-04-30 11:52:29 -0700

    Update database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,53 @@
-import { openDB } from 'idb';
 
-const initdb = async () =>
-  openDB('jate', 1, {
-    upgrade(db) {
+// import { openDB } from './idb';
+
+const initdb = () => {
+  return new Promise((resolve, reject) => {
+    const request = window.indexedDB.open('jate', 1);
+
+    request.onerror = (event) => {
+      console.error('Failed to open database', event);
+      reject(event);
+    };
+
+    request.onsuccess = (event) => {
+      console.log('Database opened successfully');
+      resolve(event.target.result);
+    };
+
+    request.onupgradeneeded = (event) => {
+      const db = event.target.result;
+
       if (db.objectStoreNames.contains('jate')) {
         console.log('jate database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+
+      const objectStore = db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+
       console.log('jate database created');
-    },
+    };
   });
+};
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const putDb = async (content) => {
+  const db = await initdb();
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  await store.add({ content });
+  await tx.complete;
+  console.log('Data added to jate database:', content);
+};
+
+export const getDb = async () => {
+  const db = await initdb();
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const data = await store.getAll();
+  await tx.complete;
+  console.log('Data retrieved from jate database:', data);
+  return data;
+};
 
 initdb();
