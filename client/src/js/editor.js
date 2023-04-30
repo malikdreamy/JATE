f1146b0d79151e92e1420c7b501b14fbf5380b4d
malikdreamy <119153047+malikdreamy@users.noreply.github.com>
2023-04-30 11:53:10 -0700

    Update editor.js

--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -1,8 +1,8 @@
-// Import methods to save and get data from the indexedDB database in './database.js'
-import { getDb, putDb } from './database';
-import { header } from './header';
+// // Import methods to save and get data from the indexedDB database in './database.js'
+import { getDb, putDb } from './database.js';
+import { header } from './header.js';
 
-export default class {
+export default class Editor {
   constructor() {
     const localData = localStorage.getItem('content');
 
@@ -21,13 +21,18 @@ export default class {
       indentUnit: 2,
       tabSize: 2,
     });
+    console.log(localData)
 
     // When the editor is ready, set the value to whatever is stored in indexeddb.
     // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
     getDb().then((data) => {
-      console.info('Loaded data from IndexedDB, injecting into editor');
-      this.editor.setValue(data || localData || header);
+      if(localData == null){
+        this.editor.setValue(header);
+        return;
+      } 
+      this.editor.setValue(localData);
     });
+
 
     this.editor.on('change', () => {
       localStorage.setItem('content', this.editor.getValue());
@@ -40,3 +45,4 @@ export default class {
     });
   }
 }
+
